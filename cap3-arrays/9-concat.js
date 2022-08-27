716596e11761fd0bcd0fb0a9b7930fa35c399fd5
Gabriel Drebtchinsky <gabrieldqcarvalho@gmail.com>
2022-08-27 15:49:06 -0300

    concat

---
+++ cap3-arrays/9-concat.js
@@ -0,0 +1,28 @@
+const zero = 0;
+const positiveNumbers = [1, 2, 3];
+const negativeNumbers = [-3, -2, -1];
+
+Array.prototype.junta = function (...arrays) {
+  const newArray = this;
+  for (let i = 0; i < arrays.length; i++) {
+    const element = arrays[i];
+    if (eArray(element)) {
+      for (let j = 0; j < element.length; j++) {
+        newArray[newArray.length] = element[j];
+      }
+    } else {
+      newArray[newArray.length] = element;
+    }
+  }
+  return newArray;
+};
+
+eArray = function (array) {
+  if (array.length) {
+    return true;
+  }
+  return false;
+};
+
+let numbers = negativeNumbers.junta(zero, positiveNumbers);
+console.log(numbers);
